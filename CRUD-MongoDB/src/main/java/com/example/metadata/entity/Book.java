9842900953f458c54a17eb73194d8ad85ed2c15d
DataVelocity <singhamr954@gmail.com>
2023-11-30 13:20:53 +0530

    Update

--- CRUD-MongoDB/src/main/java/com/example/metadata/entity/Book.java
+++ CRUD-MongoDB/src/main/java/com/example/metadata/entity/Book.java
@@ -20,37 +20,32 @@ public class Book {
  
  
  
-// public Book(String id, String name, int age, String author) {
-//	super();
-//	this.id = id;
-//	this.name = name;
-//	this.age = age;
-//	this.author=author;
-//}
-//public String getId() {
-//	return id;
-//}
-//public void setId(String id) {
-//	this.id = id;
-//}
-//public String getName() {
-//	return name;
-//}
-//public void setName(String name) {
-//	this.name = name;
-//}
-//public int getAge() {
-//	return age;
-//}
-//public void setAge(int age) {
-//	this.age = age;
-//}
-//public String getAuthor() {
-//	return author;
-//}
-//public void setAuthor(String author) {
-//	this.author = author;
-//}
+ public Book(String id, String name, int age, String author) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.age = age;
+	
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+
 
  // Constructors, getters, setters
 }
